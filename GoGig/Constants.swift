c3dd22d7cab9c7a2f5445cfd3f79cc163017214e
Lee Chilvers <lee.chilvers@icloud.com>
2019-07-11 13:07:33 +0100

    commit

---
+++ GoGig/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  GoGig
+//
+//  Created by Lee Chilvers on 26/01/2019.
+//  Copyright © 2019 ChillyDesigns. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_CREATE_ACCOUNT = "toCreateAccount"
+let TO_MAIN = "toMain"
+
+let TO_CREATE_GIG = "toCreateGig"
+let TO_TITLE_DATE = "toTitleDate"
+let TO_LOCATION_PRICING = "toLocationPricing"
+let TO_INFO_CONTACT = "toInfoContact"
+let TO_ADD_PHOTO = "toAddPhoto"
+
+let TO_FIND_GIG = "toFindGig"
+
+
+// User Defaults
+
+let DEFAULTS = UserDefaults.standard
+
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+
+
