f8cbd7135af153df6b2fd1c53242ed45ef9b68ab
Lee Chilvers <lee.chilvers@icloud.com>
2019-07-16 11:52:58 +0100

    commit

---
+++ GoGig/UserAccountUI.swift
@@ -0,0 +1,30 @@
+//
+//  UserAccountUI.swift
+//  GoGig
+//
+//  Created by Lee Chilvers on 16/07/2019.
+//  Copyright © 2019 ChillyDesigns. All rights reserved.
+//
+
+import UIKit
+
+extension UserAccountVC {
+    
+    func setupView() {
+        
+        let imageView = UIImageView(image: backgroundImage)
+        self.tableView.backgroundView = imageView
+        imageView.contentMode = .scaleAspectFit
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = imageView.bounds
+        imageView.addSubview(blurView)
+        
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.topItem?.title = "Profile"
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 350
+    }
+}
