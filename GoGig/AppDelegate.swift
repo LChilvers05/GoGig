efaf86c4a446d061780cdeda3adf1083edec8e13
Lee Chilvers <lee.chilvers@icloud.com>
2019-08-23 17:37:00 +0100

    Started Push Notification work but I think I need local notifications

--- GoGig/AppDelegate.swift
+++ GoGig/AppDelegate.swift
@@ -11,6 +11,8 @@ import Firebase
 import FirebaseAuth
 import GooglePlaces
 
+import UserNotifications
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -30,6 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window?.rootViewController?.present(loginVC, animated: true, completion: nil)
             
         }
+        
+        registerForPushNotifications()
+        
         return true
     }
     
@@ -55,6 +60,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) {
+                [weak self] granted, error in
+                
+                print("Permission granted: \(granted)")
+                
+                //guard is for when permission was not granted
+                //it returns so user does not get notifications
+                guard granted else { return }
+                self?.getNotificationSettings()
+        }
+    }
     
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+        ) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    //Team ID: SCZ5X2T8PZ
 }
 
