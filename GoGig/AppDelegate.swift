90fa1c35d5db4e6fea49dc5d4fb4726cf6426893
Lee Chilvers <lee.chilvers@icloud.com>
2019-08-27 14:28:29 +0100

    Sent a test push notification

--- GoGig/AppDelegate.swift
+++ GoGig/AppDelegate.swift
@@ -10,11 +10,12 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import GooglePlaces
-
+import FirebaseMessaging
 import UserNotifications
 
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
     
     var window: UIWindow?
     
@@ -33,7 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         }
         
+        //Push Notifications
         registerForPushNotifications()
+        Messaging.messaging().delegate = self
         
         return true
     }
@@ -60,45 +63,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    //MARK: PUSH NOTIFICATIONS
+    
     func registerForPushNotifications() {
         UNUserNotificationCenter.current()
             .requestAuthorization(options: [.alert, .sound, .badge]) {
                 [weak self] granted, error in
-                
+
                 print("Permission granted: \(granted)")
-                
+
                 //guard is for when permission was not granted
                 //it returns so user does not get notifications
                 guard granted else { return }
                 self?.getNotificationSettings()
         }
     }
-    
+
     func getNotificationSettings() {
         UNUserNotificationCenter.current().getNotificationSettings { settings in
             print("Notification settings: \(settings)")
-            
+
             guard settings.authorizationStatus == .authorized else { return }
             DispatchQueue.main.async {
                 UIApplication.shared.registerForRemoteNotifications()
             }
         }
     }
-    
+
     func application(
         _ application: UIApplication,
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
         ) {
-        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-        let token = tokenParts.joined()
-        print("Device Token: \(token)")
+        Messaging.messaging().apnsToken = deviceToken
     }
-    
+
     func application(
         _ application: UIApplication,
         didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Failed to register: \(error)")
     }
     //Team ID: SCZ5X2T8PZ
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        let dict = userInfo["aps"] as! NSDictionary
+        let message = dict["alert"]
+        print("%@", message)
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+        
+        let dataDict:[String: String] = ["token": fcmToken]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
 }
 
