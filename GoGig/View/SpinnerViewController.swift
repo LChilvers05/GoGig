ec342eab315b27ef3f8f0c034b6f9e695973e79b
Lee Chilvers <lee.chilvers@icloud.com>
2019-10-02 11:51:22 +0100

    commit

---
+++ GoGig/View/SpinnerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SpinnerViewController.swift
+//  GoGig
+//
+//  Created by Lee Chilvers on 02/10/2019.
+//  Copyright © 2019 ChillyDesigns. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .whiteLarge)
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.7)
+
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
