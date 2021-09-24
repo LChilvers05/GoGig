796b152501f2f3b0e8e89b7710738127e7ed343d
Lee Chilvers <lee.chilvers@icloud.com>
2021-09-24 16:17:43 +0100

    initial commit

---
+++ GoGig/View/LoginSignupNC.swift
@@ -0,0 +1,13 @@
+//
+//  LoginSignupNC.swift
+//  GoGig
+//
+//  Created by Lee Chilvers on 31/08/2019.
+//  Copyright © 2019 ChillyDesigns. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignupNC: UINavigationController {
+    var editingProfile = false
+}
